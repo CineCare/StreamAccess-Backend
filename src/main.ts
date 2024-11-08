adbd34187a8b668501479430defc0f7c27020623
WhitedogWarren <werquin.lucas@wanadoo.fr>
2024-11-08 22:02:49 +0100

    fix: :bug: Allow cors origin

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: { origin: true },
+  });
 
   //swagger
   const config = new DocumentBuilder()
