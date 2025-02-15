b8e6fd7f1c92ad1d79033873af7cdb31f3bbeee9
WhitedogWarren <werquin.lucas@wanadoo.fr>
2025-02-15 18:13:54 +0100

    feat: :sparkles: Manu is admin

--- src/auth/strategy/admin.strategy.ts
+++ src/auth/strategy/admin.strategy.ts
@@ -14,8 +14,10 @@ export class AdminStrategy extends PassportStrategy(Strategy, 'admin') {
   }
 
   async validate(payload: { userId: number }) {
+    const adminList = [process.env.ADMIN_EMAIL, "mr.tirmoatsu@gmail.com"]
     const user = await this.userService.getOne(payload.userId, { email: true });
-    if (!user || user.email !== process.env.ADMIN_EMAIL) {
+    //if (!user || user.email !== process.env.ADMIN_EMAIL ) {
+    if (!user || !adminList.includes(user.email)) {
       throw new UnauthorizedException();
     }
     return user;
