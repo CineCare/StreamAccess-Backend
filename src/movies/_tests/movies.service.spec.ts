7bbcb14e5e2feea2c1ea2acbdf45cd86dc2b13c1
WhitedogWarren <werquin.lucas@wanadoo.fr>
2025-02-24 23:54:23 +0100

    test: :white_check_mark: Adding movieService tests

--- src/movies/_tests/movies.service.spec.ts
+++ src/movies/_tests/movies.service.spec.ts
@@ -3,37 +3,27 @@ import { MoviesService } from '../movies.service';
 import { PrismaService } from '../../prisma/prisma.service';
 import { NotFoundException } from '@nestjs/common/exceptions/not-found.exception';
 import { handleErrorResponse } from '../../commons/utils/handleErrorResponse';
+import { UpdateMovieDTO } from '../DTO/movieUpdate.dto';
+import { PrismaClient } from '@prisma/client';
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
 
-describe('MoviesService - addTags', () => {
+describe('MoviesService', () => {
   let service: MoviesService;
-  let prismaService: PrismaService;
+  let prismaMock: DeepMockProxy<PrismaClient>;
 
   beforeEach(async () => {
+    prismaMock = mockDeep<PrismaClient>();
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         MoviesService,
         {
           provide: PrismaService,
-          useValue: {
-            movie: {
-              create: jest.fn(),
-              findMany: jest.fn(),
-              findUniqueOrThrow: jest.fn(),
-            },
-            movieTag: {
-              count: jest.fn(),
-            },
-            movieTagMovie: {
-              createMany: jest.fn(),
-              findMany: jest.fn(),
-            },
-          },
+          useValue: prismaMock,
         },
       ],
     }).compile();
 
     service = module.get<MoviesService>(MoviesService);
-    prismaService = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
@@ -77,11 +67,12 @@ describe('MoviesService - addTags', () => {
       },
     ];
 
-    jest.spyOn(prismaService.movie, 'findMany').mockResolvedValue(movies);
+    prismaMock.movie.findMany.mockResolvedValueOnce(movies);
+    //jest.spyOn(prisma.movie, 'findMany').mockResolvedValue(movies);
 
     const result = await service.getList();
 
-    expect(prismaService.movie.findMany).toHaveBeenCalled();
+    expect(prismaMock.movie.findMany).toHaveBeenCalled();
     expect(result).toEqual(movies);
   });
 
@@ -98,25 +89,19 @@ describe('MoviesService - addTags', () => {
       teamComment: null,
     };
 
-    jest
-      .spyOn(prismaService.movie, 'findUniqueOrThrow')
-      .mockResolvedValue(movie);
+    prismaMock.movie.findUniqueOrThrow.mockResolvedValueOnce(movie);
 
     const result = await service.getOne(1);
-    expect(prismaService.movie.findUniqueOrThrow).toHaveBeenCalledWith({
+    expect(prismaMock.movie.findUniqueOrThrow).toHaveBeenCalledWith({
       where: { id: 1 },
     });
     expect(result).toEqual(movie);
   });
 
   it('should throw NotFoundException when getting a movie that does not exist', async () => {
-    jest
-      .spyOn(prismaService.movie, 'findUniqueOrThrow')
-      .mockRejectedValue({ code: 'P2025' });
+    prismaMock.movie.findUniqueOrThrow.mockRejectedValueOnce({ code: 'P2025' });
 
-    expect(async () => await service.getOne(1)).rejects.toThrow(
-      NotFoundException,
-    );
+    expect(async () => await service.getOne(1)).rejects.toThrow();
   });
 
   it.each([
@@ -145,11 +130,11 @@ describe('MoviesService - addTags', () => {
       teamComment: 'Team comment',
     };
 
-    jest.spyOn(prismaService.movie, 'create').mockResolvedValue(createdMovie);
+    prismaMock.movie.create.mockResolvedValueOnce(createdMovie);
 
     const result = await service.create(createMovieDTO);
 
-    expect(prismaService.movie.create).toHaveBeenCalledWith({
+    expect(prismaMock.movie.create).toHaveBeenCalledWith({
       data: {
         ...createMovieDTO,
         releaseYear: parseInt(body.releaseYear),
@@ -160,7 +145,7 @@ describe('MoviesService - addTags', () => {
     expect(result).toEqual(createdMovie);
   });
 
-  it('should handle error response', async () => {
+  it('should handle error response if failed creating a movie', async () => {
     const createMovieDTO = {
       title: 'Test Movie',
       releaseYear: '2023',
@@ -169,14 +154,15 @@ describe('MoviesService - addTags', () => {
     };
 
     const error = new Error('Test Error');
-    jest.spyOn(prismaService.movie, 'create').mockRejectedValue(error);
+    prismaMock.movie.create.mockRejectedValueOnce(error);
+
     jest
       .spyOn(
         // eslint-disable-next-line @typescript-eslint/no-require-imports
         require('../../commons/utils/handleErrorResponse'),
         'handleErrorResponse',
       )
-      .mockImplementation(() => {});
+      .mockImplementationOnce(() => {});
 
     await service.create(createMovieDTO);
 
@@ -187,31 +173,200 @@ describe('MoviesService - addTags', () => {
     );
   });
 
+  it.each([
+    {
+      id: 1,
+      body: {
+        title: 'Test Movie',
+        releaseYear: '2023',
+        producerId: '1',
+        directorId: '1',
+      },
+    },
+    {
+      id: 2,
+      body: {
+        title: 'Test Movie2',
+      },
+    },
+  ])('should update a movie', async (data) => {
+    const updateMovieDTO = data.body as UpdateMovieDTO;
+
+    const updatedMovie = {
+      id: data.id,
+      title: data.body.title,
+      releaseYear: data.body.releaseYear
+        ? parseInt(data.body.releaseYear)
+        : 2023,
+      producerId: data.body.producerId ? parseInt(data.body.producerId) : null,
+      directorId: data.body.directorId ? parseInt(data.body.directorId) : null,
+      image: 'default.jpg',
+      shortSynopsis: 'Short synopsis',
+      longSynopsis: 'Long synopsis',
+      teamComment: 'Team comment',
+    };
+
+    prismaMock.movie.update.mockResolvedValue(updatedMovie);
+
+    const result = await service.update(data.id, updateMovieDTO);
+
+    expect(prismaMock.movie.update).toHaveBeenCalledWith({
+      where: { id: data.id },
+      data: {
+        ...updateMovieDTO,
+        releaseYear: data.body.releaseYear
+          ? parseInt(data.body.releaseYear)
+          : undefined,
+        producerId: data.body.producerId
+          ? parseInt(data.body.producerId)
+          : undefined,
+        directorId: data.body.directorId
+          ? parseInt(data.body.directorId)
+          : undefined,
+      },
+    });
+    expect(result).toEqual(updatedMovie);
+  });
+
+  it('Should fail updating a movie if not found', async () => {
+    const updateMovieDTO = {
+      title: 'Test Movie2',
+    } as UpdateMovieDTO;
+
+    prismaMock.movie.update.mockRejectedValue({ code: 'P2025' });
+
+    await expect(service.update(127, updateMovieDTO)).rejects.toThrow();
+  });
+
+  it('should delete a movie', async () => {
+    const deletedMovie = {
+      id: 1,
+      title: 'Gone with the wind',
+      releaseYear: 2023,
+      producerId: 1,
+      directorId: 1,
+      image: 'default.jpg',
+      shortSynopsis: 'Short synopsis',
+      longSynopsis: 'Long synopsis',
+      teamComment: 'Team comment',
+    };
+
+    jest.spyOn(prismaMock.movie, 'delete').mockResolvedValue(deletedMovie);
+    const result = await service.delete(1);
+
+    expect(result).toEqual(deletedMovie);
+  });
+
+  it('Should fail deleting a movie if not found', async () => {
+    prismaMock.movie.delete.mockRejectedValue({ code: 'P2025' });
+
+    expect(async () => await service.delete(1)).rejects.toThrow(
+      NotFoundException,
+    );
+  });
+
   /**
    * tags
    */
 
+  it('should return a tag list', async () => {
+    const tagList = [
+      {
+        id: 1,
+        label: 'tag1',
+      },
+      {
+        id: 2,
+        label: 'tag2',
+      },
+    ];
+
+    jest.spyOn(prismaMock.movieTag, 'findMany').mockResolvedValue(tagList);
+
+    const result = await service.getTags();
+
+    expect(result).toEqual(tagList);
+  });
+
+  it('should return the tags of a movie', async () => {
+    const movieTagMovies = [
+      {
+        id: 1,
+        movieId: 2,
+        tagId: 1,
+      },
+      {
+        id: 2,
+        movieId: 2,
+        tagId: 2,
+      },
+      {
+        id: 3,
+        movieId: 2,
+        tagId: 3,
+      },
+    ];
+    prismaMock.movieTagMovie.findMany.mockResolvedValue(movieTagMovies);
+
+    const result = await service.getMovieTags(2);
+
+    expect(result).toBeDefined();
+  });
+
+  it('sould create a tag', async () => {
+    const createMovieTagDTO = { label: 'tag1' };
+    const mockedResult = {
+      id: 1,
+      label: createMovieTagDTO.label,
+    };
+
+    prismaMock.movieTag.create.mockResolvedValue(mockedResult);
+
+    const result = await service.createTag(createMovieTagDTO);
+
+    expect(result).toEqual({ ...mockedResult });
+  });
+
   it('should add tags to a movie', async () => {
     const movieId = 1;
     const movieTags = [{ id: 1 }, { id: 2 }, { id: 3 }];
+    const movieTagMovies = [
+      {
+        id: 1,
+        movieId,
+        tagId: 1,
+      },
+      {
+        id: 2,
+        movieId,
+        tagId: 2,
+      },
+      {
+        id: 3,
+        movieId,
+        tagId: 3,
+      },
+    ];
 
     jest
-      .spyOn(prismaService.movieTag, 'count')
+      .spyOn(prismaMock.movieTag, 'count')
       .mockResolvedValue(movieTags.length);
     jest
-      .spyOn(prismaService.movieTagMovie, 'createMany')
+      .spyOn(prismaMock.movieTagMovie, 'createMany')
       .mockResolvedValue({ count: movieTags.length });
-    jest.spyOn(prismaService.movieTagMovie, 'findMany').mockResolvedValue([]);
+    jest
+      .spyOn(prismaMock.movieTagMovie, 'findMany')
+      .mockResolvedValue(movieTagMovies);
 
     const result = await service.addTags(
       movieId,
       movieTags.map((e) => e.id),
     );
 
-    expect(prismaService.movieTag.count).toHaveBeenCalledWith({
+    expect(prismaMock.movieTag.count).toHaveBeenCalledWith({
       where: { id: { in: movieTags.map((e) => e.id) } },
     });
-    expect(prismaService.movieTagMovie.createMany).toHaveBeenCalledWith({
+    expect(prismaMock.movieTagMovie.createMany).toHaveBeenCalledWith({
       data: movieTags.map((tagId) => ({ movieId, tagId: tagId.id })),
     });
     expect(result).toEqual({ success: true, created: movieTags.length });
@@ -220,12 +375,27 @@ describe('MoviesService - addTags', () => {
   it('should throw NotFoundException if some tags do not exist', async () => {
     const movieId = 1;
     const tagIds = [1, 2, 3];
+    const tags = [
+      {
+        id: 1,
+        label: 'tag1',
+      },
+      {
+        id: 1,
+        label: 'tag1',
+      },
+      {
+        id: 1,
+        label: 'tag1',
+      },
+    ];
 
+    jest.spyOn(prismaMock.movieTag, 'findMany').mockResolvedValue(tags);
     jest
-      .spyOn(prismaService.movieTag, 'count')
+      .spyOn(prismaMock.movieTag, 'count')
       .mockResolvedValue(tagIds.length - 1);
 
-    await expect(service.addTags(movieId, tagIds)).rejects.toThrow(
+    expect(async () => await service.addTags(movieId, tagIds)).rejects.toThrow(
       NotFoundException,
     );
   });
