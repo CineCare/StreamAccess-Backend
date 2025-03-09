82f5fb23babb2a275c562772328226906e185cce
WhitedogWarren <werquin.lucas@wanadoo.fr>
2025-03-09 21:43:25 +0100

    fix: :ambulance: test fix jwt in .env

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,15 +1,17 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { jwtSecret } from '../auth.module';
+//import { jwtSecret } from '../auth.module';
 import { UsersService } from '../../users/users.service';
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+require('dotenv').config();
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(private readonly userService: UsersService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: jwtSecret,
+      secretOrKey: process.env.JWT_SECRET,
     });
   }
 
