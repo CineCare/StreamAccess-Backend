d779763d05b54186b54467c9909d6d7e6df04730
WhitedogWarren <werquin.lucas@wanadoo.fr>
2025-03-15 10:56:29 +0100

    fix: :bug: Fixed PUT /movies/:id when posting an image and no title

--- src/commons/utils/fileUpload.ts
+++ src/commons/utils/fileUpload.ts
@@ -1,9 +1,30 @@
 import { extname } from 'path';
+import { PrismaService } from '../../prisma/prisma.service';
+import { MoviesService } from '../../movies/movies.service';
+import { castNumParam } from './castNumParam';
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const sanitize = require('sanitize-filename');
 
-export const editFileName = (req, file, callback) => {
-  const name = sanitize(req.body.title.replaceAll(' ', '_'));
+export const editFileName = async (req, file, callback) => {
+  let movieTitle = '';
+
+  if (req.route.stack[0].method === 'put') {
+    try {
+      const movieId = castNumParam(
+        'movieId',
+        req.url.substring(req.url.lastIndexOf('/') + 1),
+      );
+      const moviesSevice = new MoviesService(new PrismaService());
+      movieTitle = (await moviesSevice.getOne(movieId)).title;
+    } catch (e) {
+      //callback(null, false);
+      return callback(e, false);
+    }
+  } else {
+    movieTitle = req.body.title;
+  }
+
+  const name = sanitize(movieTitle.replaceAll(' ', '_'));
   const fileExtName = extname(file.originalname);
   callback(null, `${name}${fileExtName}`);
 };
