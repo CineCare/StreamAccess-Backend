0ae647115b0b7f3a79dfef0117f2e163be2cb935
WhitedogWarren <werquin.lucas@wanadoo.fr>
2025-07-09 07:37:40 +0200

    test: :white_check_mark: Tests pour HandleErrorResponse

--- src/commons/utils/_tests/handleErrorResponse.spec.ts
+++ src/commons/utils/_tests/handleErrorResponse.spec.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, UnauthorizedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { handleErrorResponse } from '../handleErrorResponse';
 describe('handleErrorResponse', () => {
   it('should return a BadRequestException', () => {
@@ -25,5 +29,37 @@ describe('handleErrorResponse', () => {
     const test = () =>
       handleErrorResponse(new Error(), 'test param', 'test value');
     expect(test).toThrow();
+  });
+
+  it('should return a NotFoundException for Prisma error P2025', () => {
+    const test = () =>
+      handleErrorResponse({ code: 'P2025' }, 'test param', 'test value');
+    expect(test).toThrow(NotFoundException);
+  });
+
+  it('should return a BadRequestException for Prisma error P2002', () => {
+    const test = () =>
+      handleErrorResponse({ code: 'P2002' }, 'test param', 'test value');
+    expect(test).toThrow(BadRequestException);
+  });
+
+  it('should return a BadRequestException for Prisma error P2003', () => {
+    const test = () =>
+      handleErrorResponse(
+        { code: 'P2003', meta: { field_name: 'test_field' } },
+        'test param',
+        'test value',
+      );
+    expect(test).toThrow(BadRequestException);
+  });
+
+  it('should return a BadRequestException for Prisma error P2000', () => {
+    const test = () =>
+      handleErrorResponse(
+        { code: 'P2000', meta: { field_name: 'test_field' } },
+        'test param',
+        'test value',
+      );
+    expect(test).toThrow(BadRequestException);
   });
 });
